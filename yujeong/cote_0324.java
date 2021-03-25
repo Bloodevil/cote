82567313cd69ec2398edcce68884bbf2a8f0e159
yujeong1789 <www_17@naver.com>
2021-03-25 17:38:09 +0900

    [유정] cote_0324 오픈채팅 수정

--- yujeong/cote_0324.java
+++ yujeong/cote_0324.java
@@ -4,48 +4,30 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class cote_0324 {
-    public static String[] solution(String[] record) {
-        String command, id, name;
+    public String[] solution(String[] record) {
         HashMap<String, String> openChat = new HashMap<String, String>(); //아이디와 닉네임을 저장할 hashMap
-        ArrayList<String> result=new ArrayList<String>(); //중간 결과를 저장할 list
+        ArrayList<String> result = new ArrayList<String>(); //중간 결과를 저장할 list
 
         for(int i=0; i<record.length; i++){
             String[] split=record[i].split(" ");
-            command=split[0];
             
-            if(command.equals("Enter")){ //Enter
-                id=split[1];
-                name=split[2];
-                openChat.put(id, name);
-                
-                result.add(id+"님이 들어왔습니다.");
-            }else if(command.equals("Leave")){ //Leave
-                id=split[1];
-
-                result.add(id+"님이 나갔습니다.");
-            }else if(command.equals("Change")){ //change
-                id=split[1];
-                name=split[2];
-
-                openChat.put(id, name);
+            if(split[0].equals("Enter")){
+                openChat.put(split[1], split[2]);
+                result.add(split[1]+"님이 들어왔습니다.");
+            }else if(split[0].equals("Leave")){
+                result.add(split[1]+"님이 나갔습니다.");
+            }else if(split[0].equals("Change")){
+                openChat.put(split[1], split[2]);
             }
-        }
+        }//for1
 
-        String[] answer=new String[result.size()];
+        String[] answer=new String[result.size()]; //list의 size로 초기화
 
         for(int j=0; j<result.size(); j++){
             String[] split2=result.get(j).split("님"); //임시로 저장된 문자열을 공백 단위로 잘라 아이디를 구한다
-            String newMsg=(result.get(j).replace(split2[0], openChat.get(split2[0])));
-            System.out.println(newMsg);
-            answer[j]=newMsg;
+            answer[j]=(result.get(j).replace(split2[0], openChat.get(split2[0]))); //id를 name으로 replace해 answer에 넣는다
         }//for2
 
         return answer;
     }
-
-    public static void main(String[] args) {
-        String[]record=new String[]{"Enter uid1234 Muzi", "Enter uid4567 Prodo", "Leave uid1234", "Enter uid1234 Prodo", "Change uid4567 Ryan"};
-        System.out.println("결과"+solution(record));
-    }
-    
 }
