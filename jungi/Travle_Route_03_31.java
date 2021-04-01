7ef41b97749df7d7029adf352893c2b363b82451
devjun63 <59526539+devjun63@users.noreply.github.com>
2021-04-01 23:35:19 +0900

    leetcode - Assign_Cookies
    기존 자바 코드의 for문에 조건을 주는것이 안되서 당황 했습니다. while에 조건을 주는 형식으로 변경했습니다.

--- jungi/Travle_Route_03_31.java
+++ jungi/Travle_Route_03_31.java
@@ -23,104 +23,69 @@ arraylist에서 중복을 제거하고 return?
 */
 import java.util.*;
 
+
+
 class Solution {
 
-    public String[] solution(String[][] tickets)
-    {
-        String start = "ICN";
-        int length = tickets.length;
-        Map<String, String> route = new HashMap<>();
-        ArrayList<String> arrivedList = new ArrayList<>();
-        /*
-        tickets[0][0] = "ICN";
-        tickets[0][1] = "JFK";
-        tickets[1][0] = "HND";
-        tickets[1][1] = "IAD";
-        tickets[2][0] = "JFK";
-        tickets[2][1] = "HND";
-        tickets[3][0] = "a";
-        tickets[3][1] = "d";
-        */
+    class Node {
+        private String index;
+        private int distance;
 
-        for(int i = 0; i < length; i++)
-        {
-            route.put(tickets[i][0],tickets[i][1]);
+        public Node(String index, int distance) {
+            this.index = index;
+            this.distance = distance;
         }
 
-        arrivedList.add(route.get("ICN"));
-        String arrived = route.get("ICN");
+        public String getIndex() {
+            return this.index;
+        }
 
+        public int getDistance() {
+            return this.distance;
+        }
+    }
 
-        arrivedList = findArrived(arrivedList, route, arrived);
+    public static final int INF = (int) 1e9; // 무한을 의미하는 값으로 10억을 설정
+    // 노드의 개수(N), 간선의 개수(M), 시작 노드 번호(Start)
+    // 노드의 개수는 최대 100,000개라고 가정
+    public static int n, m;
+    public static String start;
+    // 각 노드에 연결되어 있는 노드에 대한 정보를 담는 배열
+    public static ArrayList<ArrayList<Node>> graph = new ArrayList<ArrayList<Node>>();
+    // 방문한 적이 있는지 체크하는 목적의 배열 만들기
+    public static boolean[] visited = new boolean[100001];
+    // 최단 거리 테이블 만들기
+    public static int[] d = new int[100001];
 
-
-        ArrayList<String> answer = new ArrayList<String>();
-        for (int i = 0; i < arrivedList.size(); i++) {
-
-            if (!answer.contains(arrivedList.get(i))) {
-                if(!arrivedList.get(i).equals("ICN")) answer.add(arrivedList.get(i));
+    // 방문하지 않은 노드 중에서, 가장 최단 거리가 짧은 노드의 번호를 반환
+    public static int getSmallestNode() {
+        int min_value = INF;
+        int index = 0; // 가장 최단 거리가 짧은 노드(인덱스)
+        for (int i = 1; i <= n; i++) {
+            if (d[i] < min_value && !visited[i]) {
+                min_value = d[i];
+                index = i;
             }
         }
-        Collections.sort(answer);
-        answer.add(0, "ICN");
-        return answer.toArray(new String[answer.size()]);
+        return index;
     }
 
-    public ArrayList<String> findArrived(ArrayList<String> answer, Map<String, String> map, String arrived)
-    {
-        answer.add(arrived);
-        if(map.containsKey(map.get(arrived)))
-        {
-            arrived = map.get(arrived);
-            findArrived(answer, map, arrived);
+    public String[] solution(String[][] tickets) {
+
+        m = tickets.length;
+        start = "ICN";
+
+        // 그래프 초기화
+        for (int i = 0; i <= n; i++) {
+            graph.add(new ArrayList<Node>());
         }
-        else return answer;
-        return answer;
+
+        // 모든 간선 정보를 입력받기
+        for (int i = 0; i < m; i++) {
+            int a = 0
+            // a번 노드에서 b번 노드로 가는 비용이 c라는 의미
+            graph.get(a).add(new Node(b, c));
+        }
     }
+
 }
-
-/*
-DFS는 Depth-First Search. 깊이 우선 탐색이라고도 부르며,
-그래프에서 깊은 부분을 우선적으로 탐색하는 알고리즘이다.
-DFS를 설명하기 전에 먼저 그래프의 기본 구조를 알아야 한다.
-그래프는 **노드(Node)** 와 **간선(Edge)** 으로 표현되며
-이때 노드를 **정점(Vertex)** 이라고도 말한다.
-그래프 탐색이란 하나의 노드를 시작으로 다수의 노드를 방문하는 것을 말한다.
-또한 두 노드가 간선으로 연결되어 있다면 '두 노드는 인접하다(Adjacent)' 라고 표현한다.
-
-* 인접 행렬(Adjacency Matrix) : 2차원 배열로 그래프의 연결 관게를 표현하는 방식
-* 인접 리스트(Adjacency List) : 리스트로 그래프의 연결 관계를 표현하는 방식
-
-먼저 **인접 행렬(Adjacency Matrix)방식** 은 2차원 배열에 각 노드가 연결된 형태를 기록하는 방식이다.
-연결되어 있지 않은 노드끼리는 무한(Infinity)의 비용이라고 작성한다.
-실제 코드에서는 infinity를 999999999, 987654321 등의 값으로 정의
-
-**인접 리스트(Adjacency List) 방식**
-<br>인접 리스트에서는 '연결 리스트'라는 자료구조를 이용해 구현하는데,
-C++나 자바와 같은 프로그래밍 언어에서는 별도로 연결 리스트 기능을 위한 표준 라이브러리를 제공한다.
-반면 Python은 기본 자료형인 리스트 자료형이 append()와 메소드를 제공
-파이썬에서는 인접 리스트를 이용해 그래프를 표현하고자 할 때에도 단순히 2차원 리스트를 이용하면 됨
-
-**BFS(Breadth First Search)** 알고리즘은 '너비 우선 탐색'이라는 의미를 가진다.
-쉽게 말해 가까운 노드 부터 탐색하는 알고리즘이다. DFS가 최대한 멀리 있는 노드를 우선으로 탐색하는 방식으로 동작한다면
-BFS는 그 반대다. BFS는 선입선출 방식인 큐 자료구조를 이용하는 것이 정석이다.
-
-#### 알고리즘의 동작 방식
-* 탐색 시작 노드를 큐에 삽입하고 방문 처리를 한다.
-* 큐에서 노드를 꺼내 해당 노드의 인접 노드 중에서 방문하지 않은 노드를 모두 큐에 삽입하고 방문 처리를 한다.
-* 위 과정을 더 이상 수행할 수 없을 때까지 반복한다.
-
-너비 우선 탐색 알고리즘은 BFS는 큐 자료구조에 기초한다는 점에서 구현이 간단하다.
-실제로 구현함에 있어 앞서 언급한 대로 deque 라이브러리를 사용하는 것이 좋으며
-탐색을 수행함에 있어 O(N)의 시간이 소요된다.
-일반적인 경우 실제 수행 시간은 DFS보다 좋은 편이라는 점까지만 추가로 기억하자.
-
-### DFS / BFS 정리
-#### 동작원리
-* DFS - 스택
-* BFS - 큐
-
-#### 구현 방법
-* DFS - 재귀 함수 이용
-* BFS - 큐 자료구조 이용
-*/
