6bb03ea4b1ecbdf5a16650fdd8934501c09ef824
heestory217 <heestory217@gmail.com>
2021-04-16 23:57:28 +0900

    [희정] 0415 100. Same Tree

---
+++ heejeong/SameTree0415.java
@@ -0,0 +1,30 @@
+package heejeong;
+
+public class SameTree0415 {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p == null && q == null){
+            return true;
+        }
+        if(p == null || q == null){
+            return false;
+        }
+
+        if(p.val == q.val){
+           return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        }
+        return false;
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
