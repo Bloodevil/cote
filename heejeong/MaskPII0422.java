e6f65409baaaf58499835bd9c4fb4c8306b73161
heestory217 <heestory217@gmail.com>
2021-04-24 10:47:41 +0900

    [희정] Update MaskPII

--- heejeong/MaskPII0422.java
+++ heejeong/MaskPII0422.java
@@ -1,34 +1,27 @@
 package heejeong;
 
 public class MaskPII0422 {
-    public String maskPII(String S) {
-        // mask an email (at least 8 length)
-        int idx = S.indexOf("@");
-        if(idx != -1){
-            S = S.toLowerCase();
-            String [] names = S.split("@");
-            char first = names[0].charAt(0);
-            char last = S.charAt(idx - 1);
-            return first+"*****"+last+"@"+names[1];
-        }
-
-        // mask an phone number (at least 10 length)
-        // check the number of digit
+    public String maskEmail(String S, int idx){
+        S = S.toLowerCase();
+        String [] names = S.split("@");
+        char first = names[0].charAt(0);
+        char last = S.charAt(idx - 1);
+        return first+"*****"+last+"@"+names[1];
+    }
+    public String maskPH(String S){
         String numberOnly = S.replaceAll("[^0-9]","");
         String lastDigit = numberOnly.substring(numberOnly.length()-4);
-        switch (numberOnly.length()){
-            case 10:
-                return "***-***-"+lastDigit;
-            case 11:
-                return "+*-***-***-"+lastDigit;
-            case 12:
-                return "+**-***-***-"+lastDigit;
-            case 13:
-                return "+***-***-***-"+lastDigit;
+        if(numberOnly.length() == 10){
+            return "***-***-"+lastDigit;
+        }else{
+            return "+"+"*".repeat(numberOnly.length()-10)+"-***-***-"+lastDigit;
         }
-        return "";
     }
-
-    // 별표갯수가 lastDigit 4개 뺀만큼 있는 규칙을 이용해서
-    // 위 코드보다 더 규칙성있게 로직을 구현할 수 있을 것 같은데 떠오르지 않는다.
+    public String maskPII(String S) {
+        int idx = S.indexOf("@");
+        if(idx != -1){
+            return maskEmail(S, idx);
+        }
+        return maskPH(S);
+    }
 }
