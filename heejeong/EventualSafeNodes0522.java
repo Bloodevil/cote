518ee96fd2697135d2954212de8c393ae5a250aa
heestory217 <heestory217@gmail.com>
2021-05-22 23:05:06 +0900

    [희정] 802. Find Eventual Safe States

---
+++ heejeong/EventualSafeNodes0522.java
@@ -0,0 +1,14 @@
+package heejeong;
+
+import java.util.*;
+
+public class EventualSafeNodes0522 {
+    // 문제 이해 부족 ex2에서 왜 3은 안되는지
+    public List<Integer> eventualSafeNodes(int[][] graph) {
+        // has n Nodes (0 ~ n-1), n is graph length
+        int n = graph.length;
+        // graph[i][j] i는 0 1 2 3 4 ... n
+        // j는 i에 연결된 노드
+        // RETURN an array containing all the safe nodes (ASC)
+    }
+}
