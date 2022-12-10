65ae5348c0c95bc638f962c63cb5af6a74d96cec
jaces2 <jace@planit.ai>
2022-12-10 22:00:57 +0900

    reslove: [sort] remove duplicates 83

---
+++ yujung/cote_83_RemoveDuplicatesfromSortedList.ts
@@ -0,0 +1,15 @@
+type ListNode = {
+  val: number;
+  next: ListNode | null;
+};
+
+function deleteDuplicates(head: ListNode | null): ListNode | null {
+  if (!head || !head.next || !head.next.next) return head;
+  const copyHead = head;
+
+  while (head.next) {
+    head.val === head.next.val ? (head.next = head.next.next) : (head = head.next);
+  }
+
+  return copyHead;
+}
